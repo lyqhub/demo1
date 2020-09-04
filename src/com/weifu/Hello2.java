ef3d081316f1d09747c4882bf0de8a3ee8ee6c04
lyqhub3 <1811376694@qq.com>
2020-09-04 18:20:52 +0800

    hello2

---
+++ src/com/weifu/Hello2.java
@@ -0,0 +1,13 @@
+package com.weifu;
+
+public class Hello2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("aaa2");
+	}
+
+}
